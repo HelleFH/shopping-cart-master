ebd0ebec11ff0ad2bf825d6cdbd1214a984e0115
HelleFH <142264401+HelleFH@users.noreply.github.com>
2026-03-14 00:23:14 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:  'https://shopping-cart-master-1-5ylu.onrender.com',
+    origin:  'https://shopping-cart-master-v5l4.onrender.com',
     credentials: true,
     methods: "PUT,POST,GET,DELETE,PATCH,HEAD",
   })
